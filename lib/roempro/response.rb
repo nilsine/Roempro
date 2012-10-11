922e5483e4134199013269615f5ee343ef9cb7e8
Révillon Loïk <contact@revillon.dyndns.org>
2012-10-11 04:29:02 +0200

    Implement initialize and method_missing in Response

--- lib/roempro/response.rb
+++ lib/roempro/response.rb
@@ -3,5 +3,21 @@ require 'json'
 
 module Roempro
   class Response
+    def initialize(http_response)
+      raise "#{self.class} only support Net::HTTPResponse as input" unless http_response.kind_of? Net::HTTPResponse
+
+      @response = JSON.parse(http_response.body)
+      @response['HttpSuccess'] = http_response.is_a? Net::HTTPSuccess
+    end
+
+    def method_missing(method_id, *args)
+      raise "Unable to set values" if args.any?
+
+      return @response[method_id.to_s.split(/_/).map!(&:capitalize).join('')]
+    end
+
+    def success
+      @success
+    end
   end
 end
