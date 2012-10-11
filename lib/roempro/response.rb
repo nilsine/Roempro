25e3c0cdd1b6002d9f66e4681b9861c3a02fabfa
Révillon Loïk <contact@revillon.dyndns.org>
2012-10-11 04:36:07 +0200

    Improve exception handling

--- lib/roempro/response.rb
+++ lib/roempro/response.rb
@@ -6,16 +6,26 @@ require 'json'
 module Roempro
   class Response
     def initialize(http_response)
-      raise "#{self.class} only support Net::HTTPResponse as input" unless http_response.kind_of? Net::HTTPResponse
+      unless http_response.kind_of? Net::HTTPResponse
+        raise ArgumentError, "#{self.class} only support Net::HTTPResponse as input"
+      end
 
       @response = JSON.parse(http_response.body)
       @response['HttpSuccess'] = http_response.is_a? Net::HTTPSuccess
+
+    rescue ArgumentError => message
+      puts message
     end
 
     def method_missing(method_id, *args)
-      raise "Unable to set values" if args.any?
+      if args.any?
+        raise ArgumentError, "#{method_id.to_s} doesn't accept any agument"
+      end
 
       return @response[method_id.to_s.split(/_/).map!(&:capitalize).join('')]
+
+    rescue ArgumentError => message
+      puts message
     end
 
     def success
