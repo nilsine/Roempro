7d8ce67f82c5e77d1c126562c151ef247b3e17a4
Révillon Loïk <contact@revillon.dyndns.org>
2012-10-13 18:24:22 +0200

    Handle global session id as well as instance one

--- lib/roempro/request.rb
+++ lib/roempro/request.rb
@@ -25,11 +25,13 @@ module Roempro
     ##
     # Provide a new Roempro::Request object
     def initialize(params={})
-      @url, @username, @password = Config.url, Config.username, Config.password
-
       @url      = params[:url]      if params[:url].is_a?       String
       @username = params[:username] if params[:username].is_a?  String
       @password = params[:password] if params[:password].is_a?  String
+
+      if @url or @username or @password
+        @session_id = nil
+      end
     end
 
     def command(command_name, *args)
@@ -63,25 +65,33 @@ module Roempro
     end
 
     def logged_in?
-      @session_id ? true : false;
+      if defined? @session_id
+        @session_id ? true : false;
+      else
+        Base.session_id ? true : false;
+      end
     end
 
     def login
       unless logged_in?
-        unless @username and @password
+        unless (@username and @password) or (Config.username and Config.password)
           raise ArgumentError, "You have to submit your username and password to log into Oempro"
         end
 
         perform :command => "User.Login",
-                :username => @username,
-                :password => @password,
+                :username => @username || Config.username,
+                :password => @password || Config.password,
                 :disablecaptcha => true
 
         unless @last_response.success
           raise RuntimeError, @last_response.error_text.join("\n")
         end
 
-        @session_id = @last_response.session_id
+        if defined? @session_id
+          @session_id = @last_response.session_id
+        else
+          Base.session_id = @last_response.session_id
+        end
       end
 
     rescue ArgumentError => message
@@ -91,7 +101,7 @@ module Roempro
     private
 
       def perform(query={})
-        unless @url
+        unless @url or Config.url
           raise ArgumentError, "Unable to perform the request : Uknown URL to Oempro"
         end
         unless query.nil? or query.kind_of? Hash
@@ -99,9 +109,9 @@ module Roempro
         end
 
         query ||= {}
-        query.merge! :sessionid => @session_id, :responseformat => 'JSON'
+        query.merge! :sessionid => @session_id || Base.session_id, :responseformat => 'JSON'
 
-        uri = URI(@url)
+        uri = URI(@url || Config.url)
         uri.query = URI::encode_www_form query
 
         @last_response = Roempro::Response.new(Net::HTTP.get_response(uri))
