22468621526f605dc10b23a95b17e809ce69f109
Révillon Loïk <contact@revillon.dyndns.org>
2012-10-13 16:59:25 +0200

    Change Rails initializer to fit with the new configuration design

--- lib/rails/generators/roempro/templates/initializer.rb
+++ lib/rails/generators/roempro/templates/initializer.rb
@@ -4,4 +4,4 @@ require 'yaml'
 config_values = YAML.load_file File.join(Rails.root, 'config', 'roempro.yml')
 
 # Load the configuration into Roempro
-Roempro.load_config config_values[Rails.env.to_s]
+Roempro::Config.load_from_hash config_values[Rails.env.to_s]
